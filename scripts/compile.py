ed6f5e9cc40525455f9d2abf38e29866e1014aec
Bence Molnár <bence.molnar@snapsoft.hu>
2023-06-13 11:37:50 +0200

    compile model with pytorch on g5 instance

--- scripts/compile.py
+++ scripts/compile.py
@@ -60,7 +60,7 @@ def main(args):
     elif "inf2" in args.instance_type:
         compiled_model = compile_model_inf2(model, tokenizer, batch_size, args.num_neuron_cores)
     elif "g5" in args.instance_type:
-       compiled_model = model.to("cuda")
+       compiled_model = torch.compile(model.to("cuda"))
     else:
         raise ValueError("Unknown neuron version")
     
